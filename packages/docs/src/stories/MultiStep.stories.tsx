a0df11f9185b1cbe96a46de4212ec379e5796775
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-10-30 14:45:20 -0300

    feat(docs): add multi step documentation

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Multi Step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [(Story) => <Box>{Story()}</Box>],
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
